c3b832549580776929c0b5c2b25513e46d53518e
Pavlos Nicolaou <pauln1993@gmail.com>
2016-08-24 16:42:29 +0300

    Installing Firebase and Geofire

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Pavlos Nicolaou on 24/08/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+#import "GeoFire.h"
